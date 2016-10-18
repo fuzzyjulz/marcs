d47943c11c186c411203f4a8f196b01c57463b57
fuzzyjulz <julz.west@gmail.com>
2016-10-18 22:15:07 +1100

    Attempt to push the bucket name into one space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :marcs_image_tag
+  
+  def marcs_image_tag(*p)
+    image_tag(p).sub(/s3\.amazonaws\.com\/marcsprod/,"marcsprod.s3-ap-southeast-2.amazonaws.com").html_safe
+  end
   
     protected
   
