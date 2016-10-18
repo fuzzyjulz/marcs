d6ce949e9edbd70f79e800613425d8a5be3e7a9b
fuzzyjulz <julz.west@gmail.com>
2016-10-18 22:59:16 +1100

    Doing some prettying up

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,11 +1,12 @@
 function jqueryload_ready(){
 	//Load pages if you put the load tag on it
 	$(".jqueryLoad").each(function (key) {
-		$(this).html("Loading...");
   var url = $(this).attr("href");
+  loaderDiv = $(this);
+  loaderDiv.html("<div class='badge'>Loading</div>");
   $(this).load(url, function(response,status){
    if ($(this).size() == 0 || status != "success") {
-   	$(this).html("<div class='alert alert-danger' role='alert'>Error loading page.</div>");
+   	loaderDiv.html("<div class='alert alert-danger' role='alert'>Error loading page.</div>");
    }
   });
 	});
