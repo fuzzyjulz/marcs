726683db7dc9d8208ed6c98726dce3654cd79ef8
fuzzyjulz <julz.west@gmail.com>
2016-11-25 00:05:24 +1100

    Pretty up newsfeed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  helper_method :marcs_image_tag
+  helper_method :marcs_image_tag, :extract_links
   
   def marcs_image_tag(*p)
     ActionController::Base.helpers.image_tag(*p).sub(/s3\.amazonaws\.com\/marcsprod/,"marcsprod.s3-ap-southeast-2.amazonaws.com").html_safe
+  end
+  
+  def extract_links(string)
+    string.gsub(/(https?:[\/\.\w\d\-?]+)/,"<a href='\\1'>\\1</a>").html_safe
   end
   
     protected
