3a325cc3fd0ba9666baa71329a49eaa49924523c
fuzzyjulz <julz.west@gmail.com>
2017-05-25 02:14:41 +1000

    limit renewals to Julian

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,11 @@ class Ability
     unless user.nil?
       can [:view_committee_members, :view_member_area], User
       if user.financial?
-        can [:view_club_trainers, :view_minutes, :renew_membership], User
+        can [:view_club_trainers, :view_minutes], User
         can [:view_club_minutes,:view_agm_minutes], Minutes
+        if user.first_name.downcase == "julian"
+          can [:renew_membership], User
+        end
         if user.committee_member?
           can [:view_committee_calendar], User
           can [:view_committee_minutes], Minutes
