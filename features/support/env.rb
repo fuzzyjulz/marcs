31a4710d33d28d97f78573fd69dd0ee8f08fcb3b
fuzzyjulz <julz.west@gmail.com>
2017-10-30 21:52:23 +1100

    increase minimum code coverage!

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,7 @@ require 'colorize'
 begin
   require 'simplecov'
   SimpleCov.start 'test_frameworks'
-  SimpleCov.minimum_coverage 65
+  SimpleCov.minimum_coverage 75
   puts "Simple Cov Started.".green
 rescue LoadError
   puts "Simple Cov test engine not installed.".red
